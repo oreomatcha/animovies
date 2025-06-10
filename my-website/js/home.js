093158310841dad02c4e49509ec9dc4b69073c22
oreomatcha <rainluisx@gmail.com>
2025-06-10 14:25:39 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = '34b7989ede454343719237b16c9ba650';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
